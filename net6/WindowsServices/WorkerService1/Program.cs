ee096c252d3227899cb309ac8348c237802ea36f
Giuseppe Forese <Giuseppe.Forese@ditechspa.it>
2021-11-21 11:53:38 +0100

    creation

--- net6/WindowsServices/WorkerService1/Program.cs
+++ net6/WindowsServices/WorkerService1/Program.cs
@@ -1,10 +1,29 @@
 using WorkerService1;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
 
-await host.RunAsync();
+try
+{
+    IHost host = Host.CreateDefaultBuilder(args)
+        .UseWindowsService(options =>
+        {
+            options.ServiceName = "WorkerSevice (.Net 6.0)";
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddHostedService<Worker>();
+        })
+        .Build();
+
+    await host.RunAsync();
+
+}
+catch (Exception ex)
+{
+   //todo
+}
+finally
+{
+    //todo
+}
+
+
--- net6/WindowsServices/WorkerService1/Worker.cs
+++ net6/WindowsServices/WorkerService1/Worker.cs
@@ -13,7 +13,7 @@ namespace WorkerService1
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Ciao Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
